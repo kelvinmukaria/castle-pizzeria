7edc5b74b069a8f5eda1f1720573437c8eb5b371
kingsam91 <lambetgm@gmail.com>
2020-05-28 14:02:32 +0300

    add checkbox for delivery

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $("document").ready(function() {
-  $(".submit").click(function() {
+  $(".submit").click(function(event) {
+    event.preventDefault();
     var size = $("#size").val();
     var crust = $("#crust").val();
     var quantity = $("#quantity").val();
     var sizeprice = 0;
 
-    if (size = "small") {
+    if (size === "small") {
       sizeprice = 500;
-    } else if (size = "medium") {
+    } else if (size === "medium") {
       sizeprice = 750;
-    } else if (size = "large") {
+    } else if (size === "large") {
       sizeprice = 1000;
     }
 
@@ -20,8 +21,24 @@ $("document").ready(function() {
       toppings = toppings + amount;
 
     });
+    //
+    //     $('input[type="checkbox"]').on("click", getLocation);
+    //
+    //
+    // function get(){
+    //      if($(this).prop('checked') == true){
+    //      document.getElementById('text').innerHTML+=$(this).val();
+    //      }
+    //      else{
+    //      var text = document.getElementById('text').innerHTML;
+    //      text=  text.replace($(this).val(),'');
+    //      document.getElementById('text').innerHTML = text;
+    //      }
+    //
+    // }
 
-var totalPrice = (toppings + sizeprice + parseInt(crust))*quantity;
+    var totalPrice = (toppings + sizeprice + parseInt(crust)) * quantity;
     alert(totalPrice)
+
   });
 })
